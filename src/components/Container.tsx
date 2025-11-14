7bd6c1df6fd604a68a2a97317cd2765547fdba3a
Leutrim Dema <LeutrimD1@gmail.com>
2025-11-14 12:53:21 -0600

    fix over scroll?

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,14 +3,11 @@ import { styled } from "@mui/material"
 const StyledContainer = styled("div")({
     display: 'flex',
     flexDirection: 'column',
-    marginLeft: '10%',
-    marginRight: '10%',
-    maxWidth: '100vw',
-    overflowX: 'hidden',
-    boxSizing: "border-box",
-    minHeight: '100dvh',
-    transform: 'translateZ(0)',
-    WebkitOverflowScrolling: 'touch'
+    marginLeft: '20%',
+    marginRight: '20%',
+    overscrollBehavior: 'none',  // Prevents overscroll
+    WebkitOverflowScrolling: 'touch',
+    overflowX: 'hidden'
 })
 
 export default function Container({ children } : { children: React.ReactNode}) {
